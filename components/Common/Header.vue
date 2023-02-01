cecee7c25a4f5ae57702ded3b11360df69045600
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-01 16:57:04 +0530

    added Base Header

---
+++ components/Common/Header.vue
@@ -0,0 +1,20 @@
+<template>
+	<v-app-bar clipped-left fixed app>
+		<v-card color="design" class="py-4 px-12">Logo</v-card>
+		<v-spacer />
+		<v-avatar color="design" size="36"> </v-avatar>
+	</v-app-bar>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
