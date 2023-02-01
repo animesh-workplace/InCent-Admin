228e6b5b20ccac8af88cad692b2fac64ec3e6d93
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-01 16:57:29 +0530

    added Users for main page

---
+++ components/Application/Users.vue
@@ -0,0 +1,86 @@
+<template>
+	<v-container class="mt-8">
+		<v-sheet outlined class="py-4 px-6 rounded-lg">
+			<div class="d-flex align-center mt-n8 ml-8">
+				<v-icon class="design--text mr-1">mdi-account</v-icon>
+				<span class="design--text font-weight-medium text-h6 white">Users</span>
+			</div>
+			<div class="mt-8">
+				<h4 class="grey--text font-weight-regular pl-10">4 Users added</h4>
+
+				<v-simple-table class="mx-6 my-4 rounded-lg elevation-2">
+					<template v-slot:default>
+						<thead>
+							<tr>
+								<th class="text-left">Sr No.</th>
+								<th class="text-left">Name</th>
+								<th class="text-left">Designation</th>
+								<th class="text-left">Mobile no.</th>
+								<th class="text-left">Email ID</th>
+								<th class="text-left">Profile</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr
+								:class="index % 2 ? 'grey lighten-3' : 'white'"
+								v-for="(item, index) in desserts"
+								:key="item.name"
+							>
+								<td>{{ index + 1 }}</td>
+								<td>{{ item.name }}</td>
+								<td>{{ item.designation }}</td>
+								<td>{{ item.mobile }}</td>
+								<td>{{ item.email }}</td>
+								<td>{{ item.profile }}</td>
+							</tr>
+						</tbody>
+					</template>
+				</v-simple-table>
+			</div>
+		</v-sheet>
+	</v-container>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		desserts: [
+			{
+				name: 'Vedant Parte',
+				designation: 'IT',
+				mobile: '7458963254',
+				email: 'vedantaparte77@gmail.com',
+				profile: 'seller',
+			},
+			{
+				name: 'Vedant Parte',
+				designation: 'IT',
+				mobile: '7458963254',
+				email: 'vedantaparte77@gmail.com',
+				profile: 'seller',
+			},
+			{
+				name: 'Vedant Parte',
+				designation: 'IT',
+				mobile: '7458963254',
+				email: 'vedantaparte77@gmail.com',
+				profile: 'seller',
+			},
+			{
+				name: 'Vedant Parte',
+				designation: 'IT',
+				mobile: '7458963254',
+				email: 'vedantaparte77@gmail.com',
+				profile: 'seller',
+			},
+		],
+	}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
