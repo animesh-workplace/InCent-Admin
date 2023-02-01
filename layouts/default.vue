7bcf93feed6e863b7cdb4681c356ac4440991355
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-01 16:56:43 +0530

    added default layout

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-app>
+		<CommonSideBar />
+		<CommonHeader />
+		<v-main>
+			<Nuxt />
+		</v-main>
+		<CommonFooter />
+	</v-app>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
