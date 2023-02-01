c445b85b57559d4d9c39c0cb65b9b345dea4571b
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-01 16:57:20 +0530

    added BreadCrumb for paths

---
+++ components/Common/BreadCrumb.vue
@@ -0,0 +1,24 @@
+<template>
+	<v-sheet class="py-8 px-16">
+		<h1 class="font-weight-medium design--text">ABC Company</h1>
+		<v-breadcrumbs :items="items" divider=">" class="px-0 pt-0"></v-breadcrumbs>
+	</v-sheet>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		items: [
+			{ text: 'Applications', disabled: false },
+			{ text: 'Application No. 111111', disabled: false },
+		],
+	}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
