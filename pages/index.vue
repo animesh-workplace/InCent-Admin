54cf8ff06a90d406889bf037d3fc8537cbf755b0
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-02 16:22:57 +0530

    added index page that redirects to admin

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script>
+export default {
+	data: () => ({}),
+	components: {},
+	methods: {},
+	beforeMount() {
+		this.$router.push('/admin')
+	},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
