96c2d13bc52a0da9e31cc3ced069a1c08f8b508d
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-02 15:20:55 +0530

    added app-bar-icon

--- components/Common/Header.vue
+++ components/Common/Header.vue
@@ -1,5 +1,6 @@
 <template>
 	<v-app-bar clipped-left fixed app>
+		<v-app-bar-nav-icon />
 		<v-card color="design" class="py-2 py-lg-4 px-12">Logo</v-card>
 		<v-spacer />
 		<v-avatar color="design" size="36"> </v-avatar>
