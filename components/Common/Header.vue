cd8006110975bc141ecec2fab044681a05f5b79a
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-02 12:24:53 +0530

    logo padding-y changing from 4 to 12 as lg-device hits

--- components/Common/Header.vue
+++ components/Common/Header.vue
@@ -1,6 +1,6 @@
 <template>
 	<v-app-bar clipped-left fixed app>
-		<v-card color="design" class="py-4 px-12">Logo</v-card>
+		<v-card color="design" class="py-2 py-lg-4 px-12">Logo</v-card>
 		<v-spacer />
 		<v-avatar color="design" size="36"> </v-avatar>
 	</v-app-bar>
