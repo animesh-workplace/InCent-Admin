beca6279d5d672c23576abd223732b7de17a65ce
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-02 13:54:56 +0530

    added dynamic company page

---
+++ pages/company/_company.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<CommonBreadCrumb />
+		<ApplicationHeader />
+		<ApplicationCompany />
+		<!-- 		<ApplicationUsers />
+		<v-sheet class="pa-8 mt-4">
+			<v-container class="text-center">
+				<v-btn elevation="2" outlined rounded color="success" class="mr-6">
+					<span class="text-body-2">Approve</span>
+				</v-btn>
+				<v-btn elevation="2" outlined rounded color="error" class="mr-6">
+					<span class="text-body-2">Reject with comment</span>
+				</v-btn>
+			</v-container>
+		</v-sheet> -->
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
