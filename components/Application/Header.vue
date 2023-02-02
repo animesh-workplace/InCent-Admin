c451f5cc70f76d47834a0c5d7a219c8f91199fd0
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-02 12:43:04 +0530

    removed commented section

--- components/Application/Header.vue
+++ components/Application/Header.vue
@@ -37,28 +37,6 @@
 						</div>
 					</v-col>
 				</v-row>
-				<!-- 				<div class="d-flex flex-row justify-space-around">
-					<div class="mx-4 text-center">
-						<h3 class="subtitle-2 grey--text">Time</h3>
-						<h3 class="subtitle-1">4:14 am</h3>
-					</div>
-					<div class="mx-4 text-center">
-						<h3 class="subtitle-2 grey--text">Date</h3>
-						<h3 class="subtitle-1">16/2/23</h3>
-					</div>
-					<div class="mx-4 text-center">
-						<h3 class="subtitle-2 grey--text">Status</h3>
-						<h3 class="subtitle-1 red--text"><v-icon>mdi-book-cancel</v-icon> Rejected</h3>
-					</div>
-					<div class="mx-4 text-center">
-						<h3 class="subtitle-2 grey--text">Attempt</h3>
-						<h3 class="subtitle-1">2<sup>nd</sup> Attempt</h3>
-					</div>
-					<div class="mx-4 text-center">
-						<h3 class="subtitle-2 grey--text">Comments (1)</h3>
-						<h3 class="subtitle-1 blue--text">View previous comment</h3>
-					</div>
-				</div> -->
 			</v-container>
 		</v-sheet>
 	</div>
