19b6a6dd68dd625bc81f2c036437665a9d434a5a
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-02 12:28:46 +0530

    Testing responsiveness by removing flex and adding grid

--- components/Application/Header.vue
+++ components/Application/Header.vue
@@ -5,7 +5,39 @@
 		</div>
 		<v-sheet class="grey lighten-3 mt-6 py-5">
 			<v-container>
-				<div class="d-flex flex-row justify-space-around">
+				<v-row>
+					<v-col>
+						<div class="mx-4 text-center">
+							<h3 class="subtitle-2 grey--text">Time</h3>
+							<h3 class="subtitle-1">4:14 am</h3>
+						</div>
+					</v-col>
+					<v-col>
+						<div class="mx-4 text-center">
+							<h3 class="subtitle-2 grey--text">Date</h3>
+							<h3 class="subtitle-1">16/2/23</h3>
+						</div>
+					</v-col>
+					<v-col>
+						<div class="mx-4 text-center">
+							<h3 class="subtitle-2 grey--text">Status</h3>
+							<h3 class="subtitle-1 red--text"><v-icon>mdi-book-cancel</v-icon> Rejected</h3>
+						</div>
+					</v-col>
+					<v-col>
+						<div class="mx-4 text-center">
+							<h3 class="subtitle-2 grey--text">Attempt</h3>
+							<h3 class="subtitle-1">2<sup>nd</sup> Attempt</h3>
+						</div>
+					</v-col>
+					<v-col>
+						<div class="mx-4 text-center">
+							<h3 class="subtitle-2 grey--text">Comments (1)</h3>
+							<h3 class="subtitle-1 blue--text">View previous comment</h3>
+						</div>
+					</v-col>
+				</v-row>
+				<!-- 				<div class="d-flex flex-row justify-space-around">
 					<div class="mx-4 text-center">
 						<h3 class="subtitle-2 grey--text">Time</h3>
 						<h3 class="subtitle-1">4:14 am</h3>
@@ -26,7 +58,7 @@
 						<h3 class="subtitle-2 grey--text">Comments (1)</h3>
 						<h3 class="subtitle-1 blue--text">View previous comment</h3>
 					</div>
-				</div>
+				</div> -->
 			</v-container>
 		</v-sheet>
 	</div>
