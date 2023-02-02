68c19eb4d9a4afa741a40e3afcc44b9d9c9fb8bf
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-02 15:21:27 +0530

    testing other button

--- components/Common/SideBar.vue
+++ components/Common/SideBar.vue
@@ -1,22 +1,28 @@
 <template>
-	<v-navigation-drawer clipped fixed app class="elevation-4" color="design" v-model="visible">
-		<div class="d-flex justify-end">
-			<v-btn icon right color="white" @click="HideNav">
-				<v-icon>mdi-chevron-double-left</v-icon>
-			</v-btn>
-		</div>
+	<div>
+		<v-navigation-drawer clipped fixed app class="elevation-4" color="design" v-model="visible">
+			<div class="d-flex justify-end">
+				<v-btn icon right color="white" @click="HideNav">
+					<v-icon>mdi-chevron-double-left</v-icon>
+				</v-btn>
+			</div>
 
-		<v-list>
-			<v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact class="white--text">
-				<v-list-item-action>
-					<v-icon color="white">{{ item.icon }}</v-icon>
-				</v-list-item-action>
-				<v-list-item-content>
-					<v-list-item-title v-text="item.title" />
-				</v-list-item-content>
-			</v-list-item>
-		</v-list>
-	</v-navigation-drawer>
+			<v-list>
+				<v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact class="white--text">
+					<v-list-item-action>
+						<v-icon color="white">{{ item.icon }}</v-icon>
+					</v-list-item-action>
+					<v-list-item-content>
+						<v-list-item-title v-text="item.title" />
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
+
+		<!-- 		<v-btn icon right color="red" class="float-right mt-16">
+			<v-icon>mdi-chevron-double-right</v-icon>
+		</v-btn> -->
+	</div>
 </template>
 
 <script>
