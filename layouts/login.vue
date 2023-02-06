d082288ce99fb8d10777dc86593c75a326143c4a
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-06 16:02:31 +0530

    added new layout for signup page

---
+++ layouts/login.vue
@@ -0,0 +1,19 @@
+<template>
+	<v-app>
+		<CommonHeader login />
+		<Nuxt />
+	</v-app>
+</template>
+
+<script>
+export default {
+	data: () => ({}),
+	components: {},
+	methods: {},
+	mounted() {
+		this.$nextTick(() => {})
+	},
+}
+</script>
+
+<style></style>
