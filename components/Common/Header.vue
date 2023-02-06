0778c06f4d65987ce0bb45a267218493e93b8828
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-06 16:03:12 +0530

    Reusing component for signup page

--- components/Common/Header.vue
+++ components/Common/Header.vue
@@ -1,15 +1,16 @@
 <template>
-	<v-app-bar clipped-left fixed app>
-		<v-app-bar-nav-icon />
-		<v-card color="design" class="py-2 py-lg-4 px-12">Logo</v-card>
-		<v-spacer />
-		<v-avatar color="design" size="36"> </v-avatar>
+	<v-app-bar clipped-left fixed app color="white">
+		<v-app-bar-nav-icon v-if="!login" />
+		<img src="@/assets/logo.png" alt="Logo" height="60px" />
+		<v-spacer v-if="!login" />
+		<v-avatar color="design" size="36" v-if="!login"> </v-avatar>
 	</v-app-bar>
 </template>
 
 <script>
 export default {
 	data: () => ({}),
+	props: { login: { default: false, type: Boolean } },
 	components: {},
 	methods: {},
 	mounted() {
