fddd60a8b92d77e5d3abc21ddaf2eabd57efd512
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-06 16:01:44 +0530

    added multiple styles for testing

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,20 @@
-<template></template>
+<template>
+	<v-sheet>
+		<v-container>
+			<v-file-input class="d-block" placeholder="Im label" prepend-icon="mdi-file-pdf-box"></v-file-input>
+			<!-- <v-file-input chips prepend-icon="" prepend-inner-icon="" placeholder="Im label"></v-file-input> -->
+		</v-container>
+	</v-sheet>
+</template>
 
 <script>
 export default {
 	data: () => ({}),
+	layout: 'login',
 	components: {},
 	methods: {},
 	beforeMount() {
-		this.$router.push('/admin')
+		// this.$router.push('/admin')
 	},
 	mounted() {
 		this.$nextTick(() => {})
@@ -14,4 +22,4 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped></style>
