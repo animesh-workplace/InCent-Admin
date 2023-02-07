815333cb108d621ea8078873d89fbea6167cfd18
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-07 16:18:15 +0530

    removed container

--- pages/admin.vue
+++ pages/admin.vue
@@ -1,139 +1,139 @@
 <template>
-	<v-container>
-		<v-sheet class="py-4 px-6">
-			<div class="text-left mb-6">
-				<h2 class="text-h5 design--text mb-4">Company Application</h2>
-				<h3 class="caption">
-					These page can be used by Invecent support team to check company application status
-				</h3>
-			</div>
+	<!-- <v-container class="px-0 px-sm-16"> -->
+	<v-sheet class="py-4 px-8">
+		<div class="text-left mb-6">
+			<h2 class="text-h5 design--text mb-4">Company Application</h2>
+			<h3 class="caption">
+				These page can be used by Invecent support team to check company application status
+			</h3>
+		</div>
 
-			<div class="high-margin">
-				<v-text-field class="search py-2 pl-6" hide-details placeholder="Search Application">
-					<v-icon slot="prepend" color="design" class="mt-0"> mdi-magnify </v-icon>
-				</v-text-field>
-			</div>
+		<div class="high-margin">
+			<v-text-field class="search py-2 pl-6" hide-details placeholder="Search Application">
+				<v-icon slot="prepend" color="design" class="mt-0"> mdi-magnify </v-icon>
+			</v-text-field>
+		</div>
 
-			<div class="mt-8">
-				<div class="d-flex justify-space-between align-center px-10">
-					<div>
-						<h4 class="grey--text font-weight-regular">4 Users added</h4>
-					</div>
-					<div>
-						<v-btn outlined color="grey" class="px-2">
-							<span class="button_text mr-2">sort on values</span>
-							<img width="16" alt="Sort" src="@/assets/sort_icon.png" />
-						</v-btn>
-					</div>
+		<div class="mt-8">
+			<div class="d-flex justify-space-between align-center px-10">
+				<div>
+					<h4 class="grey--text font-weight-regular">4 Users added</h4>
 				</div>
-
-				<v-simple-table class="mx-6 my-4 rounded-lg elevation-2">
-					<template v-slot:default>
-						<thead>
-							<tr>
-								<th class="text-left">Sr No.</th>
-								<th class="text-left">Application No.</th>
-								<th class="text-left">Registered Company Name</th>
-								<th class="text-left">Date<br />Time</th>
-								<th class="text-left">Status</th>
-								<th class="text-left">Comment</th>
-								<th class="text-left">Action</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr
-								:class="index % 2 ? 'grey lighten-3' : 'white'"
-								v-for="(item, index) in desserts"
-								:key="item.name"
-							>
-								<td>{{ index + 1 }}</td>
-								<td>{{ item.app_no }}</td>
-								<td>{{ item.company }}</td>
-								<td>
-									{{ item.date }} <br />
-									{{ item.time }}
-								</td>
-								<td>
-									<div v-if="item.status == 'Approved'">
-										<div class="d-flex">
-											<img
-												width="22"
-												height="22"
-												alt="Sucess"
-												class="mr-3"
-												src="@/assets/icon_success.png"
-											/>
-											<div>
-												<span class="success--text text-caption font-weight-light">
-													{{ item.status }}
-												</span>
-											</div>
-										</div>
-									</div>
-									<div v-if="item.status == 'Pending'">
-										<div class="d-flex">
-											<img
-												width="22"
-												height="22"
-												alt="Sucess"
-												class="mr-3"
-												src="@/assets/icon_pending.png"
-											/>
-											<div>
-												<span class="grey--text text-caption font-weight-light">
-													{{ item.status }}
-												</span>
-											</div>
-										</div>
-									</div>
-									<div v-if="item.status == 'Rejected'">
-										<div class="d-flex">
-											<img
-												width="22"
-												height="22"
-												alt="Sucess"
-												class="mr-3"
-												src="@/assets/icon_reject.png"
-											/>
-											<div>
-												<span class="error--text text-caption font-weight-light">
-													{{ item.status }}
-												</span>
-											</div>
-										</div>
-									</div>
-								</td>
-								<td>
-									<div class="text-center">
-										<h6 class="grey--text font-weight-regular text-subtitle-2">NA</h6>
-										<h6 class="grey--text font-weight-regular">(Not applicable)</h6>
-									</div>
-								</td>
-								<td>
-									<v-btn
-										block
-										small
-										color="design"
-										class="text-caption text-capitalize"
-										@click="ChangePage(item.link)"
-									>
-										<span class="mr-2 white--text">View Application</span>
-										<img
-											width="20"
-											height="20"
-											alt="Sucess"
-											class="mt-1"
-											src="@/assets/eye_icon.png"
-										/>
-									</v-btn>
-								</td>
-							</tr>
-						</tbody>
-					</template>
-				</v-simple-table>
+				<div>
+					<v-btn outlined color="grey" class="px-2">
+						<span class="button_text mr-2">sort on values</span>
+						<img width="16" alt="Sort" src="@/assets/sort_icon.png" />
+					</v-btn>
+				</div>
 			</div>
-		</v-sheet>
-	</v-container>
+
+			<v-simple-table class="mx-6 my-4 rounded-lg elevation-2">
+				<template v-slot:default>
+					<thead>
+						<tr>
+							<th class="text-left">Sr No.</th>
+							<th class="text-left">Application No.</th>
+							<th class="text-left">Registered Company Name</th>
+							<th class="text-left">Date<br />Time</th>
+							<th class="text-left">Status</th>
+							<th class="text-left">Comment</th>
+							<th class="text-left">Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr
+							:class="index % 2 ? 'grey lighten-3' : 'white'"
+							v-for="(item, index) in desserts"
+							:key="item.name"
+						>
+							<td>{{ index + 1 }}</td>
+							<td>{{ item.app_no }}</td>
+							<td>{{ item.company }}</td>
+							<td>
+								{{ item.date }} <br />
+								{{ item.time }}
+							</td>
+							<td>
+								<div v-if="item.status == 'Approved'">
+									<div class="d-flex">
+										<img
+											width="22"
+											height="22"
+											alt="Sucess"
+											class="mr-3"
+											src="@/assets/icon_success.png"
+										/>
+										<div>
+											<span class="success--text text-caption font-weight-light">
+												{{ item.status }}
+											</span>
+										</div>
+									</div>
+								</div>
+								<div v-if="item.status == 'Pending'">
+									<div class="d-flex">
+										<img
+											width="22"
+											height="22"
+											alt="Sucess"
+											class="mr-3"
+											src="@/assets/icon_pending.png"
+										/>
+										<div>
+											<span class="grey--text text-caption font-weight-light">
+												{{ item.status }}
+											</span>
+										</div>
+									</div>
+								</div>
+								<div v-if="item.status == 'Rejected'">
+									<div class="d-flex">
+										<img
+											width="22"
+											height="22"
+											alt="Sucess"
+											class="mr-3"
+											src="@/assets/icon_reject.png"
+										/>
+										<div>
+											<span class="error--text text-caption font-weight-light">
+												{{ item.status }}
+											</span>
+										</div>
+									</div>
+								</div>
+							</td>
+							<td>
+								<div class="text-center">
+									<h6 class="grey--text font-weight-regular text-subtitle-2">NA</h6>
+									<h6 class="grey--text font-weight-regular">(Not applicable)</h6>
+								</div>
+							</td>
+							<td>
+								<v-btn
+									block
+									small
+									color="design"
+									class="text-caption text-capitalize"
+									@click="ChangePage(item.link)"
+								>
+									<span class="mr-2 white--text">View Application</span>
+									<img
+										width="20"
+										height="20"
+										alt="Sucess"
+										class="mt-1"
+										src="@/assets/eye_icon.png"
+									/>
+								</v-btn>
+							</td>
+						</tr>
+					</tbody>
+				</template>
+			</v-simple-table>
+		</div>
+	</v-sheet>
+	<!-- </v-container> -->
 </template>
 
 <script>
@@ -187,8 +187,8 @@ export default {
 	border-radius: 46px;
 }
 .high-margin {
-	margin-left: 84px;
-	margin-right: 84px;
+	margin-left: px;
+	margin-right: px;
 }
 .button_text {
 	color: #929292;
