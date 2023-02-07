0e8a13f1cac65d1cabfbb8ad36c5ef92c48938a1
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-07 11:42:48 +0530

    avatar updated

--- components/Common/Header.vue
+++ components/Common/Header.vue
@@ -1,9 +1,11 @@
 <template>
 	<v-app-bar clipped-left fixed app color="white" :height="login ? 90 : undefined">
-		<v-app-bar-nav-icon v-if="!login" />
+		<!-- <v-app-bar-nav-icon v-if="!login" /> -->
 		<img src="@/assets/logo.png" alt="Logo" :height="login ? '80px' : '60px'" />
 		<v-spacer v-if="!login" />
-		<v-avatar color="design" size="36" v-if="!login"> </v-avatar>
+		<v-avatar color="design" size="36" v-if="!login">
+			<img src="@/assets/avatar.png" alt="Avatar" />
+		</v-avatar>
 	</v-app-bar>
 </template>
 
