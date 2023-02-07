523d7f8a59b08a169649915b02a0df85273b54e0
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-07 11:17:54 +0530

    testing drawer stuff

--- layouts/login.vue
+++ layouts/login.vue
@@ -4,11 +4,24 @@
 		<Nuxt />
 		<CommonFooter />
 	</v-app>
+	<!-- 	<v-app>
+		<v-navigation-drawer v-model="drawer" app>
+		</v-navigation-drawer>
+		<v-toolbar fixed class="transparent">
+			<v-btn icon @click.stop="drawer = !drawer">
+				<v-icon>
+					{{ drawer ? 'mdi-chevron-right' : 'mdi-chevron-left' }}
+				</v-icon>
+			</v-btn>
+		</v-toolbar>
+	</v-app> -->
 </template>
 
 <script>
 export default {
-	data: () => ({}),
+	data: () => ({
+		drawer: true,
+	}),
 	components: {},
 	methods: {},
 	mounted() {
