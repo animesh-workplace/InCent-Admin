349db2f3d8ee9faaee2f22e39d208b5d6e519e42
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-07 08:15:22 +0530

    updated the design and added rules for validation

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,63 @@
 <template>
-	<v-sheet class="margin-high mx-auto rounded-lg" elevation="4">
-		<v-container>
-			<v-row>
-				<v-col align-self="center">
-					<img src="@/assets/hero.png" alt="Hero" />
-				</v-col>
-				<v-col>
-					<v-text-field outlined persistent-hint label="Mobile No./ Email"></v-text-field>
-					<v-text-field outlined persistent-hint label="Password" hide-details></v-text-field>
-					<v-btn rounded color="design" dark class="mt-4"> Login </v-btn>
-				</v-col>
-			</v-row>
-		</v-container>
+	<v-sheet class="margin-high mx-auto rounded-lg center" elevation="4">
+		<v-row>
+			<v-col align-self="center" class="border-col">
+				<img src="@/assets/hero.png" alt="Hero" />
+			</v-col>
+			<v-col class="pa-4">
+				<v-container class="center mt-10">
+					<span class="text-label text-h6">Mobile No./ Email</span>
+					<v-text-field
+						outlined
+						class="mt-2"
+						v-model="payload.user_id"
+						:rules="[rules.required, rules.min]"
+						placeholder="Enter your own Mobile No./ Email"
+					/>
+					<span class="text-label text-h6">Password</span>
+					<v-text-field
+						outlined
+						password
+						class="mt-2"
+						:rules="[rules.required]"
+						placeholder="Enter your Password"
+						:type="show_password ? 'text' : 'password'"
+						@click:append="show_password = !show_password"
+						:append-icon="show_password ? 'mdi-eye' : 'mdi-eye-off'"
+					/>
+					<div class="center text-center">
+						<v-btn rounded block color="design" dark x-large class="mt-4"> Login </v-btn>
+					</div>
+				</v-container>
+			</v-col>
+		</v-row>
 	</v-sheet>
 </template>
 
 <script>
 export default {
-	data: () => ({}),
+	data: () => ({
+		payload: {
+			user_id: '',
+			password: '',
+		},
+		show_password: false,
+		rules: {
+			required: (value) => !!value || 'Required.',
+			min: (v) => v.length >= 10 || 'Min 10 characters',
+			email: (v) => /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(v) || 'Please enter correct mail',
+			phoneemail: (input) => {
+				const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+				const phoneRegex = /^\d{10}$/
+
+				if (!emailRegex.test(input)) {
+					return 'invalid email'
+				} else if (!phoneRegex.test(input)) {
+					return 'invalid phone number'
+				}
+			},
+		},
+	}),
 	layout: 'login',
 	components: {},
 	methods: {},
@@ -33,5 +73,20 @@ export default {
 <style scoped>
 .margin-high {
 	margin-top: 100px;
+}
+.center {
+	width: 70%;
+	margin: auto;
+	padding: 10px;
+}
+.border-col {
+	border-right: 2px solid #c4c4c4;
+}
+.text-label {
+	color: #26467e;
+	font-size: 14px;
+	font-weight: 400;
+	line-height: 17px;
+	font-style: normal;
 }
 </style>
