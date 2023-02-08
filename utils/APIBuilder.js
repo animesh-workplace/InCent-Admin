faa798b01f01744db9adcaebffa99cb96a0ee49e
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-08 18:34:53 +0530

    added Api builder

---
+++ utils/APIBuilder.js
@@ -0,0 +1,93 @@
+import axios from 'axios'
+import Vue from './sessionManager'
+
+const vue = new Vue()
+
+function buildGetRequest(url) {
+	var headers = {}
+	if (vue.isUserLoggedIn()) {
+		headers['authorization'] = 'Bearer ' + vue.getToken()
+	}
+	return axios.get(url, { headers: headers })
+}
+
+function buildGetRequestWithData(url, data) {
+	var headers = {}
+	if (vue.isUserLoggedIn()) {
+		console.log('User LoginIN')
+		headers['authorization'] = 'Basic ' + vue.getToken()
+	}
+	return axios({
+		url: url,
+		method: 'get',
+		headers: headers,
+		params: data,
+	})
+}
+
+function buildGetRequestWithResponseType(url, responseType) {
+	var headers = {}
+	if (vue.isUserLoggedIn()) {
+		headers['authorization'] = 'Bearer ' + vue.getToken()
+	}
+	return axios({
+		method: 'get',
+		url: url,
+		responseType: responseType,
+	})
+}
+
+function buildPostRequest(url, data) {
+	var headers = {}
+	if (vue.isUserLoggedIn()) {
+		headers['authorization'] = 'Bearer ' + vue.getToken()
+	}
+	return axios.post(url, data, { headers: headers })
+}
+
+function buildFilePostRequest(url, data) {
+	var headers = {}
+	if (vue.isUserLoggedIn()) {
+		headers['authorization'] = 'Bearer ' + vue.getToken()
+	}
+	headers['Content-Type'] = 'multipart/form-data'
+
+	return axios.post(url, data, { headers: headers })
+}
+
+function buildPutRequest(url, data) {
+	var headers = {}
+	if (vue.isUserLoggedIn()) {
+		headers['authorization'] = 'Bearer ' + vue.getToken()
+	}
+
+	return axios.put(url, data, { headers: headers })
+}
+// query para put method
+function buildPutRequestWithData(url, data) {
+	var headers = {}
+	if (vue.isUserLoggedIn()) {
+		headers['authorization'] = 'Bearer ' + vue.getToken()
+	}
+
+	return axios.put(url, data, { headers: headers, params: data })
+}
+
+function buildDeleteRequest(url) {
+	var headers = {}
+	if (vue.isUserLoggedIn()) {
+		headers['authorization'] = 'Bearer ' + vue.getToken()
+	}
+	return axios.delete(url, { headers: headers })
+}
+
+export {
+	buildGetRequest,
+	buildGetRequestWithResponseType,
+	buildPutRequest,
+	buildPostRequest,
+	buildFilePostRequest,
+	buildDeleteRequest,
+	buildGetRequestWithData,
+	buildPutRequestWithData,
+}
