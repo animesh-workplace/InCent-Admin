f6ea98d35eb030bcb15acdf2ba8f6763f8ace57a
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-08 22:43:07 +0530

    added new URL

--- utils/URLConstant.js
+++ utils/URLConstant.js
@@ -1,5 +1,6 @@
 const BASE_URL = 'http://15.206.222.49:3000/api'
 const LOGINDATA = `${BASE_URL}/incentUser/loginIncentUser`
 const COMPANYLIST = `${BASE_URL}/incentUser/getAllRegisteredCompanyListByStatus`
+const COMPANYLISTAPPROVAL = `${BASE_URL}/incentUser/updateRequestedCompanyStatus`
 
-export default { LOGINDATA, COMPANYLIST }
+export default { LOGINDATA, COMPANYLIST, COMPANYLISTAPPROVAL }
