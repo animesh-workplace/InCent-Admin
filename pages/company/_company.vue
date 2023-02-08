63b3b94de599536651b2c165b0d83c3dbccf1cce
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-08 22:42:58 +0530

    added props for users

--- pages/company/_company.vue
+++ pages/company/_company.vue
@@ -3,7 +3,7 @@
 		<CommonBreadCrumb :application="company_details.application_no" />
 		<ApplicationHeader :info="company_details" />
 		<ApplicationCompany :info="company_details" />
-		<ApplicationUsers />
+		<ApplicationUsers :users="company_user_details" />
 		<ApplicationApproval />
 	</div>
 </template>
@@ -14,12 +14,14 @@ import * as company from '@/utils/api/RequestCompanyAPI'
 export default {
 	data: () => ({
 		company_details: {},
+		company_user_details: {},
 	}),
 	layout: 'admin',
 	components: {},
 	methods: {},
 	beforeMount() {
 		this.company_details = company.default.getRequestedCompany()
+		this.company_user_details = company.default.getRequestedCompanyUsers()
 	},
 	mounted() {
 		this.$nextTick(() => {})
