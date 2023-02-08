65c8730cd064b0e5d66c5c749d58f4cff79401f0
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-08 19:29:00 +0530

    aded company list url

--- utils/URLConstant.js
+++ utils/URLConstant.js
@@ -1,4 +1,5 @@
 const BASE_URL = 'http://15.206.222.49:3000/api'
 const LOGINDATA = `${BASE_URL}/incentUser/loginIncentUser`
+const COMPANYLIST = `${BASE_URL}/incentUser/getAllRegisteredCompanyListByStatus`
 
-export default { LOGINDATA }
+export default { LOGINDATA, COMPANYLIST }
