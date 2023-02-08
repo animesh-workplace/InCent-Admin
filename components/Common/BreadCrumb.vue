34cb2767f67dc49777b1b3179c6fe7c8af3e6425
Animesh Kumar Singh <animesh.workplace@gmail.com>
2023-02-08 19:56:38 +0530

    removed hard-coded items

--- components/Common/BreadCrumb.vue
+++ components/Common/BreadCrumb.vue
@@ -1,18 +1,21 @@
 <template>
 	<v-sheet class="py-8 px-8 px-sm-16">
 		<h1 class="font-weight-medium design--text">ABC Company</h1>
-		<v-breadcrumbs :items="items" divider=">" class="px-0 pt-0"></v-breadcrumbs>
+		<v-breadcrumbs
+			:items="[
+				{ text: 'Applications', disabled: false },
+				{ text: `Application No. ${application}`, disabled: false },
+			]"
+			divider=">"
+			class="px-0 pt-0"
+		></v-breadcrumbs>
 	</v-sheet>
 </template>
 
 <script>
 export default {
-	data: () => ({
-		items: [
-			{ text: 'Applications', disabled: false },
-			{ text: 'Application No. 111111', disabled: false },
-		],
-	}),
+	data: () => ({}),
+	props: { application: { type: Number, required: true } },
 	components: {},
 	methods: {},
 	mounted() {
